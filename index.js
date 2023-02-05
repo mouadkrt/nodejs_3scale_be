94fe5a4a9001e5ed6479257b0e6f0368bcc0c670
Sentissi Mouad <m.sentissi@gmail.com>
2023-02-05 09:38:49 +0100

    Using bodyParser.text({type: '*/*'})

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ var app = express();
 
 
 const bodyParser = require('body-parser');
-require('body-parser-xml')(bodyParser);
-app.use(bodyParser.xml({inflate:true}));
+//require('body-parser-xml')(bodyParser);
+//app.use(bodyParser.xml({inflate:true}));
 
 var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 
-app.post('/', function(req, res) {
+app.post('/', bodyParser.text({type: '*/*'}), function(req, res) {
 
     console.log("\n\n-------------------------\nIAMSAPReceiptTransferExport_V1_BE received the following HTTP GET Request on / endpoint  :");
 
@@ -21,9 +21,12 @@ app.post('/', function(req, res) {
 
     console.log("\nHTTP BODY : %j", req.body);
 
-    const xml_content = fs.readFileSync("IAMSAPReceiptTransferExport_V1_BE_reply.xml");
-    res.setHeader('content-type', 'text/xml; charset=utf-8');
-    res.send(xml_content);
+    //const xml_content = fs.readFileSync("IAMSAPReceiptTransferExport_V1_BE_reply.xml");
+    //res.setHeader('content-type', 'text/xml; charset=utf-8');
+    //res.send(xml_content);
+    newBackendBoy = req.body + "\n(This string is added by NodeJs backend to req.body)";
+    console.log("Sending : \n" + newBackendBoy);
+    res.send(newBackendBoy);
 
 
 });
